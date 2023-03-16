0b9cfc1bbbe422c4936411ee0cfdcd37622cfc1b
yassinerahhou <yassine.rahhou.1@gmail.com>
2023-03-16 09:37:13 -0700

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,11 @@
+#include<stdio.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
-}
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}}
