a220440bf24de2376272a596651daefdf7f08175
yassinerahhou <yassine.rahhou.1@gmail.com>
2023-03-21 07:39:44 -0700

    second

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,10 @@
 #include "main.h"
-/*
-* print_alphabet - Prints all lowercase letters of the English alphabet
+
+/**
+* print_alphabet - print all alphabet in lowercase
 */
+
+
 void print_alphabet(void)
 {
 char letter;
