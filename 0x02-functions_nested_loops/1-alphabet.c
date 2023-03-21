7f06780c7c13caa635f43fcf9fba6d8035785366
yassinerahhou <yassine.rahhou.1@gmail.com>
2023-03-21 07:18:44 -0700

    second

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* main - print _putchar
+* Yassin
+* Return: 0
+*/
+void print_alphabet(void)
+{
+char letter;
+
+
+for (letter = 'a'; letter <= 'z'; letter++)
+_putchar(letter);
+
+
+_putchar('\n');
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,4 +1,5 @@
 #ifndef main_h
 #define main_h
 int _putchar(char);
+void print_alphabet(void);
 #endif
