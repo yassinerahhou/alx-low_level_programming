349eead0813694ce25d8b7145c720e64f80f7661
yassinerahhou <yassine.rahhou.1@gmail.com>
2023-03-21 05:24:37 -0700

    fist cod

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
-/*main - print _putchar
+/*
+*main - print _putchar
 * is written by Yacin
 * Return: 0
 */
 int main()
 {
-    printf("_putchar\n");
-    return 0;
+printf("_putchar\n");
+return 0;
 }
