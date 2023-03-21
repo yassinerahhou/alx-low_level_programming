b4cd58b4d807aa9e3d835346a9236647ff9e9ab9
yassinerahhou <yassine.rahhou.1@gmail.com>
2023-03-21 10:18:26 -0700

    3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _islower - checks if a character is a lowercase letter in ASCII
+ * @c: the character to check
+ *
+ * Return: 1 if c is a lowercase letter, 0 otherwise
+ */
+int _islower(int c)
+{
+        if (c >= 97  && c <= 122)
+        return (1);
+        else
+        return (0);
