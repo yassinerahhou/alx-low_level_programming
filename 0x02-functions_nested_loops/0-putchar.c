807776df92e01845bb2415a8dff09ff991428011
yassinerahhou <yassine.rahhou.1@gmail.com>
2023-03-21 06:33:46 -0700

    first

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,19 @@
-#include <unistd.h>
-/*
-*main - print _putchar
-* is written by Yacin
+#include "main.h"
+/**
+* main - print _putchar
+* Yassin
 * Return: 0
 */
 int main(void)
 {
-printf("_putchar\n");
-return 0;
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
 }
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,4 @@
+#ifndef main_h
+#define main_h
+int _putchar(char);
+#endif
