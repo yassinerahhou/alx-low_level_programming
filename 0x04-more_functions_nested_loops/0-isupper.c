6ebb8a347e4c99d6eb4e3dc17fa49d3c2dfa8322
yassinerahhou <yassine.rahhou.1@gmail.com>
2023-03-23 06:30:44 -0700

    checker

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _isupper - hecks for uppercase character
+ * @c: the character to check
+ *
+ * Return: 1 if c is a lowercase letter, 0 otherwise
+ */
+int _isupper(int c)
+{
+if (c >= 65  && c <= 90)
+return (1);
+else
+{
+return (0);
+}
+}
