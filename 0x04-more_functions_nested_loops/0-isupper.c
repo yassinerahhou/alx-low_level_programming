55f8dea677cdc6cfa75d65567fb5b1dfd4abeec4
yassinerahhou <yassine.rahhou.1@gmail.com>
2023-03-23 07:07:07 -0700

    0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,16 +1,11 @@
 #include "main.h"
+
 /**
- * _isupper - hecks for uppercase character
- * @c: the character to check
- *
- * Return: 1 if c is a lowercase letter, 0 otherwise
+ * _isupper - checks for uppercase character
+ * @c: the character to be checked
+ * Return: 1 if c is uppercase, 0 otherwise
  */
 int _isupper(int c)
 {
-if (c >= 65  && c <= 90)
-return (1);
-else
-{
-return (0);
-}
+	return (c >= 'A' && c <= 'Z');
 }
--- 0x04-more_functions_nested_loops/_putchar.c
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -1,5 +1,5 @@
+include "main.h"
 #include <unistd.h>
-
 /**
  * _putchar - writes the character c to stdout
  * @c: The character to print
@@ -9,5 +9,5 @@
  */
 int _putchar(char c)
 {
-	return (write(1, &c, 1));
+ return (write(1, &c, 1));
 }
