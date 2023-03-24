9e8cc5abfd769c119ba3d34bbbc42e57a8dd58cf
yassinerahhou <yassine.rahhou.1@gmail.com>
2023-03-23 17:44:29 -0700

    6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -6,18 +6,18 @@
  */
 void print_line(int n)
 {
-if (n <= 0)
-{
-_putchar('\n');
-} else
-{
-int i;
+	if (n <= 0)
+	{
+		_putchar('\n');
+	} else
+	{
+		int i;
 
-for (i = 1; i <= n; i++)
-{
-_putchar('_');
-}
-_putchar('\n');
+		for (i = 1; i <= n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
 	}
 
 }
