587ee34b1e62e61172c93a0100c220ed52553865
yassinerahhou <yassine.rahhou.1@gmail.com>
2023-03-27 08:02:36 -0700

    4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_rev - Prints a string in reverse
+ * @s: string
+ * Return: void
+ */
+void print_rev(char *s)
+{
+for (int i = 15; i >= 0 ; i--)
+{
+_putchar(s[i]);
+}
+    _putchar('\n');
+    }
