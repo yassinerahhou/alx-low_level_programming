866564ff691de62fc92f5a594b59acd7caa9b6b0
yassinerahhou <yassine.rahhou.1@gmail.com>
2023-04-03 18:17:45 -0700

    more pointers, arrays and strings

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,19 +1,29 @@
-unsigned int _strspn(char *s, char *accept) {
-unsigned int i, y, count = 0;
-for (i = 0; s[i]; i++) {
-printf("loop s[%d]: %c \n",i,s[i]);
-for (y = 0; accept[y]; y++) {
-printf("loop accept[%d]: %c \n",y,accept[y]);
-if (s[i] == accept[y]) {
-printf("s[%d] = accept[%d]: %c = %c\n",i,y,s[i],accept[y]);
-count++;
-printf("count: %d \n",count);
-break;
-}
-}
-if (accept[y] == '\0'){
-printf("accept[%d]: %c exit loops return 0: %d \n",y, accept[y]);
-break;}
-}
-return (count);
+#include "main.h"
+/**
+ * _strspn - function that gets the length of a prefix substring.
+ * @s:  a pointer to  string
+ * @accept: This is a pointer to the string  //yassine rahhou
+ * Return: number of bytes in the initial segment
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int len, i, j;
+
+	len = 0;
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				len++;
+				break;
+			}
+		}
+		if (accept[j] == '\0')
+		{
+			return (len);
+		}
+	}
+	return (len);
 }
