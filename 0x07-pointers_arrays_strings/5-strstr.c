e75afe7d5e620d6ebbfb08070d3cbad5aab101c6
yassinerahhou <yassine.rahhou.1@gmail.com>
2023-04-04 10:43:04 -0700

    more pointers, arrays and strings

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _strstr - finds the first occurrence of the substring needle haystack
+ * @haystack: char
+ * @needle: char
+ *
+ * Return: needle or NULL (SUCCESS)
+ */
+char *_strstr(char *haystack, char *needle)
+{
+int i, y;
+for (i = 0 ; haystack[i] != '\0' ; i++)
+{
+for (y = 0 ; needle[y] != '\0' ; y++)
+if (haystack[i] == needle[y])
+{
+return (needle);
+}
+}
+return (0);
+}
