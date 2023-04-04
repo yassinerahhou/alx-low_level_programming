4b7b0145f8a4f66c74286fba5d124cc07cfbe416
yassinerahhou <yassine.rahhou.1@gmail.com>
2023-04-04 15:43:06 -0700

    more pointers, arrays and strings

---
+++ 0x07-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,16 @@
+ (12 sloc)  247 Bytes
+ 
+
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
