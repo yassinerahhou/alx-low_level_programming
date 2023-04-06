1038b3ce2a4e96b3a0574b79bc26b47859e11333
yassinerahhou <yassine.rahhou.1@gmail.com>
2023-04-06 15:58:47 -0700

    Recursion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,15 +1,21 @@
 #include "main.h"
 int _sqrt(int n, int i);
-
 /**
- * power_operation - returns the natural square root of a number.
- * @n: input number.
- * @c: iterator.
- * Return: square root or -1.
+ * _sqrt_value - search for the square value of n beginning
+ * @n: number
+ * @value:value
+ * Return: natural square value //zakaria elaroussi
  */
-int _sqrt_recursion(int n)
+
+int _sqrt_value(int n, int value)
 {
-return (_sqrt(n, 1));
+if (value * value > n)
+return (-1);
+
+if (value * value == n)
+return (value);
+
+return (_sqrt_value(n, value + 1));
 }
 /**
  * _sqrt - Entry Function
