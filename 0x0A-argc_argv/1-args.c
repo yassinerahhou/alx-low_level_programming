5c1ff18d6f08cd832e44b6d925a8634059fedd6a
yassinerahhou <yassine.rahhou.1@gmail.com>
2023-04-08 20:57:54 -0700

    2

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/* By_Yacin */
+/**
+ * main - prints its name, followed by a new line.
+ * @argc: number dlcomandeline flargements .
+ * @argv:larrayslik ayhsb lines .
+ * Return : 0 always succses .
+*/
+int main(int argc, char __attribute__((unused)) *argv[])
+{
+printf("%s\n", argc - 1);
+return (0);
+}
