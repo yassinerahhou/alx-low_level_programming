296818da04822f4d820339fd9af7a37b06ad214f
yassinerahhou <yassine.rahhou.1@gmail.com>
2023-04-09 09:42:54 -0700

    33

---
+++ 3-mul.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - multiplies two numbers.
+ * @argc: number of arguments
+ * @argv: number of arguments
+ * Return: either 0 or 
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	return (0);
+}
