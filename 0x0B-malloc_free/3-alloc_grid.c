555ac3a185f0171c11556182bde358a9ce98ed8d
yassinerahhou <yassine.rahhou.1@gmail.com>
2023-04-12 19:34:56 -0700

    wake

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,40 @@
+#include <stdlib.h>
+
+/**
+ * alloc_grid -  function that returns 
+ * @width: chat 
+ * @height: chat
+ * Return: always 0
+ */
+int **alloc_grid(int width, int height)
+{
+int **grid;
+int f, w;
+
+if (width <= 0 || height <= 0)
+return (NULL);
+
+grid = malloc(sizeof(int *) * height);
+if (grid == NULL)
+return (NULL);
+
+for (f = 0; f < height; f++)
+{
+grid[f] = malloc(sizeof(int) * width);
+if (grid[f] == NULL)
+{
+for (w = 0; w < f; w++)
+{
+free(grid[w]);
+}
+free(grid);
+return (NULL);
+}
+for (w = 0; w < width; w++)
+{
+grid[f][w] = 0;
+}
+}
+
+return (grid);
+}
