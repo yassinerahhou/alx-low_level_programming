5bdf815de88864f35d2cdc5becc7d0c35088ba0a
yassinerahhou <yassine.rahhou.1@gmail.com>
2023-04-13 19:40:49 -0700

    string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,35 +1,41 @@
-#include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
-
 /**
- * *_strdup - function
- * @str: char
- * Return: return 0 or ptr
+ * *string_nconcat - main function
+ * @s1: string 1
+ * @s2: string 2
+ * @n: string 3
+ * Return: always 0
  */
 
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+char *str;
+unsigned int p1;
+unsigned int p2;
+unsigned int x;
+unsigned int y;
 
-char *_strdup(char *str)
-
-{
-char *copy_s;
-int x;
-int y;
-if (str == NULL)
-{
-return (0);
-}
-for (x = 0; str[x] != '\0'; x++)
+if (!s1)
+s1 = "";
+if (!s2)
+s2 = "";
+for (x = 0; s1[x] != '\0'; x++)
 {
 }
-copy_s = malloc((x + 1) * sizeof(char));
-if (copy_s == NULL)
+for (y = 0; s2[y] != '\0' && y < n; y++)
 {
-return (0);
 }
-for (y = 0; y <= x; y++)
+str = malloc(sizeof(char) * (x + y + 1));
+if (!str)
+return (NULL);
+for (p1 = 0; p1 < x; p1++)
 {
-copy_s[y] = str[y];
+str[p1] = s1[p1];
 }
-return (copy_s);
+for (p2 = 0; p2 < y; p2++)
+{
+str[p1 + p2] = s2[p2];
+}
+str[p1 + p2] = '\0';
+return (str);
 }
