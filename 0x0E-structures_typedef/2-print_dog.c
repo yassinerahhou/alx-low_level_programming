3422197d1bfa767bf33b309c330c9e64e11324c4
yassinerahhou <yassine.rahhou.1@gmail.com>
2023-04-17 15:21:57 -0700

    4

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -6,18 +6,22 @@
  * @d: struct
  * Return: no return.
  */
-void print_dog(struct dog *d)
-if (d == NULL)
-return;
-{
-if (d->name == NULL)
-printf("Name: (nil)\n");
-else
-printf("Name: %s\n", d->name);
-printf("Age: %f\n", d->age);
-if (d->owner == NULL)
-printf("Owner: (nil)\n");
-else
-printf("Owner: %s\n", d->owner);
-}
+void print_dog(struct dog *d) {
+    if (d == NULL) {
+        return;
+    }
+
+    if (d->name == NULL) {
+        printf("Name: (nil)\n");
+    } else {
+        printf("Name: %s\n", d->name);
+    }
+
+    printf("Age: %f\n", d->age);
+
+    if (d->owner == NULL) {
+        printf("Owner: (nil)\n");
+    } else {
+        printf("Owner: %s\n", d->owner);
+    }
 }
