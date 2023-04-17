fd576f0db880bdcee640ad298462318f2d3e1db7
yassinerahhou <yassine.rahhou.1@gmail.com>
2023-04-17 08:23:11 -0700

    Function-like macro

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,6 @@
+#ifndef ABS_H
+#define ABS_H
+
+#define ABS(x) ((x) < 0 ? ((x) * -1) : (x))
+
+#endif
---
+++ 0x0D-preprocessor/3-main.c
@@ -0,0 +1,20 @@
+julien@ubuntu:~/0x0c. macro, structures$ cat 3-main.c
+#include <stdio.h>
+#include "3-function_like_macro.h"
+#include "3-function_like_macro.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    int i;
+    int j;
+
+    i = ABS(-98) * 10;
+    j = ABS(98) * 10;
+    printf("%d, %d\n", i, j);
+    return (0);
+}
