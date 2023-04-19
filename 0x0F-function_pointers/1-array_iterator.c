4e48d40d8223da4e7841c213af9395de05084f85
yassinerahhou <yassine.rahhou.1@gmail.com>
2023-04-19 15:21:00 -0700

    you'll never get it done

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -12,8 +12,11 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 unsigned int i;
 if (array == NULL || action == NULL)
-for  (i = 0; i < size ; i++)
 {
-	action(array[i]);
+return;
+}
+for (i = 0; i < size; i++)
+{
+action(array[i]);
 }
 }
