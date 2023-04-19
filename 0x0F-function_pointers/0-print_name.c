62c87441dde1e980ce8834f685319f55ff6e7a13
yassinerahhou <yassine.rahhou.1@gmail.com>
2023-04-19 10:56:31 -0700

    0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+#include <stdio.h>
+/**
+ * print_name - print name fonction
+ * @name: the name
+ * @f: pointurs to funtion
+ * Return: nothing
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+if (name == NULL || f == NULL)
+return;
+f(name);
+}
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,5 +1,7 @@
-#ifndef function_pointers.h_H
-#define function_pointers.h_H
+#ifndef function_pointers
+#define function_pointers
 void print_name(char *name, void (*f)(char *));
-
+void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
+int _putchar(char c);
 #endif
