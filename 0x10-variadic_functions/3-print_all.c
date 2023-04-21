21d6b9fc56073cca2a0dc470021477ae9bb0bf71
yassinerahhou <yassine.rahhou.1@gmail.com>
2023-04-21 09:18:14 -0700

    pusher

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -8,9 +8,10 @@
 void print_all(const char * const format, ...)
 {
 int i = 0;
-char *str;
-va_list args;
-va_start(args, format);
+char *st;
+va_list argsz;
+
+va_start(argsz, format);
 while (format == NULL)
 {
 printf("\n");
@@ -18,30 +19,32 @@ return;
 }
 while (format[i] != 0)
 {
-witch (format[i])
+switch (format[i])
 {
 case 99:
-printf("%c", (char) va_arg(args, int));
+putchar((char) va_arg(argsz, int));
 break;
 case 105:
-printf("%d", va_arg(args, int));
+printf("%d", va_arg(argsz, int));
 break;
 case 102:
-printf("%f", (float) va_arg(args, double));
+printf("%f", (float) va_arg(argsz, double));
 break;
 case 115:
-str = (char *) va_arg(args, char *);
-if (str == NULL)
+st = (char *) va_arg(argsz, char *);
+if (st == NULL)
 {
 printf("(nil)");
 break;
 }
-printf("%s", str);
+printf("%s", st);
 break;
 }
 if ((format[i] == 99 || format[i] == 102 || format[i] == 105 ||
 format[i] == 115) && format[(i + 1)] != '\0')
 printf(", ");
 i++;
-}printf("\n");va_end(args);
+}
+printf("\n");
+va_end(argsz);
 }
