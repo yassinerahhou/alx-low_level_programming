7c5cf3b22416d0ce58f5193152839a12ea595937
yassinerahhou <yassine.rahhou.1@gmail.com>
2023-04-27 08:52:47 -0700

    Print list

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,19 +1,17 @@
 #ifndef LIST_H
-#define LISTS_H
-
+#define LIST_H
 /**
- * listh description
  * struct list_s - singly linked list
  * @str: string - malloced strings
  * @len: length string
- * @next: points to the next 
+ * @next: points to the next
  *
  */
 typedef struct list_s
 {
-	char *str;
-	unsigned int len;
-	struct list_s *next;
+char *str;
+unsigned int len;
+struct list_s *next;
 } list_t;
 
 size_t print_list(const list_t *h);
