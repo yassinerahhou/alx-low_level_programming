6659ead6282791260ff61a1a6dd1cc5d7f8e61a7
yassinerahhou <yassine.rahhou.1@gmail.com>
2023-04-27 09:11:11 -0700

    List length

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -6,7 +6,7 @@
 #include "lists.h"
 /**
  * list_len - function that returns the number of list
- * @h: A pointer to the head node 
+ * @h: A pointer to the head node
  * Return: bacck to count_n
  */
 size_t list_len(const list_t *h)
