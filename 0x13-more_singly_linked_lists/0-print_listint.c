d931602eb8f9790bc64e7ffe7d9934d4935fa999
yassinerahhou <yassine.rahhou.1@gmail.com>
2023-04-29 05:44:51 -0700

    print list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -3,15 +3,15 @@
  * print_listint - print all .
  * @h: pointer .
  * size_t : size of rawdaw .
- * listint_t: 
+ * listint_t: list iin .
  * Return: back to int.
  **/
-size_t print_listint(const listint_t *h) 
+size_t print_listint(const listint_t *h)
 {
 	size_t rawdaw = 0;
-	while (h != NULL)
+while (h != NULL)
 {
-	printf("%d\n", h->n);	
+	printf("%d\n", h->n);
 		h = h->next;
 	rawdaw++;
 }
