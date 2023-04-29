eee240ea85821036542e5a3e9ab6dc0c52cba754
yassinerahhou <yassine.rahhou.1@gmail.com>
2023-04-29 05:50:36 -0700

    print list

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -8,14 +8,14 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * 
  */
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *next;
+int n;
+struct listint_s *next;
 } listint_t;
-/** project's prototype **/size_t print_listint(const listint_t *h);
+/** project's prototype **/
+size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
 listint_t *add_nodeint_end(listint_t **head, const int n);
