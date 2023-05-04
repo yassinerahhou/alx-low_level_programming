fd9f304c5dfede85039cb3721cbcf4113211e97f
yassinerahhou <yassine.rahhou.1@gmail.com>
2023-05-04 09:59:14 -0700

    first push

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -5,15 +5,15 @@
  * @b: string containing numbers
  *
  * Return: converted number
- * */
+ **/
 unsigned int binary_to_uint(const char *b)
 {
 	int i;
 	unsigned int res_val = 0;
 if (!b)
 return (0);
-for (i = 0; b[i]; i++)
-	{
+	for (i = 0; b[i]; i++)
+{
 		if (b[i] < '0' || b[i] > '1')
 			return (0);
 		res_val = 2 * res_val + (b[i] - '0');
