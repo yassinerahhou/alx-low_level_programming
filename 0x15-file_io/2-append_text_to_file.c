c9102aded671dea21ba84f66cbda7347068344d7
yassinerahhou <yassine.rahhou.1@gmail.com>
2023-05-09 09:53:12 -0700

    last

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,36 +1,37 @@
 #include "main.h"
 
 /**
- * append_text_to_file - appends text
- * @filename: name of the file
- * @text_content: the NULL terminated string to add
- * Return: back to 1 or -1
+ * append_text_to_file - appends text at the end
+ * @filename: file name
+ * @text_content: the NULL of context
+ * Return: back to 1 if else -1
  */
 
 int append_text_to_file(const char *filename, char *text_content)
 {
-int _f, _write, text_len = 0;
-if (!(filename))
-{
-return (-1);
+int _f, _w, text_len = 0;
+
+	if (!(filename))
+	{
+		return (-1);
 	}
-_f = open(filename, O_WRONLY, O_APPEND);
-if (_f == -1)
-{
-return (-1);
-}
-if (text_content)
-{
-while (text_content[text_len])
-{
-text_len++;
-}
-_write = write(_f, text_content, text_len);
-if (_write == -1)
-{
-return (-1);
-}
-}
-close(_f);
-return (1);
+	_f = open(filename, O_WRONLY, O_APPEND);
+	if (_f == -1)
+	{
+		return (-1);
+	}
+	if (text_content)
+	{
+		while (text_content[text_len])
+		{
+			text_len++;
+		}
+		_w = write(_f, text_content, text_len);
+		if (_w == -1)
+		{
+			return (-1);
+		}
+	}
+	close(_f);
+	return (1);
 }
