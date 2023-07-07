ea1c5edfbf27fd621b233e27bd730c2818492367
yassinerahhou <yassine.rahhou.1@gmail.com>
2023-07-07 16:29:50 -0700

    xx

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,26 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - function
+ * @ht: A pointer to the hash table.
+ * @key: The key to get the value of.
+ * Return: key - NULL.
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	hash_node_t *node;
+	unsigned long int index;
+
+	if (ht == NULL || key == NULL || *key == '\0')
+		return (NULL);
+
+	index = key_index((const unsigned char *)key, ht->size);
+	if (index >= ht->size)
+		return (NULL);
+
+	node = ht->array[index];
+	while (node && strcmp(node->key, key) != 0)
+		node = node->next;
+
+	return ((node == NULL) ? NULL : node->value);
+}
