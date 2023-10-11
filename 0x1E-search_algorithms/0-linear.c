5dec5aa344de771f7ed83373a7f732d3b6ec05c9
yassinerahhou <yassine.rahhou.1@gmail.com>
2023-10-11 07:21:20 -0700

    Linear search algo

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -16,6 +16,7 @@ int linear_search(int *array, size_t size, int value)
 	{
 		if (array[i] == value)
 		{
+			printf("Value checked array[%lu] = %d\n", (unsigned long)i, array[i]);
 			return (i);
 		}
 	}
