2b914dc6a5b54f0b03949754b3c312fa786e389a
yassinerahhou <yassine.rahhou.1@gmail.com>
2023-10-12 10:28:21 -0700

    jumpsearch funtion

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -1,1 +1,18 @@
-x
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+int jump_search(int *array, size_t size, int value)
+{
+    size_t i ,start = 0, end= sqrt(size);
+    while(array[end]< value && start<size){
+        start = end ;
+        end = end + sqrt(size);
+        if (end >size-1) end =size;
+        
+    }
+    for (i=start;i <= end ; i++){
+        if(array[i]== value)
+        return i;
+    }
+    return -1;
+}
